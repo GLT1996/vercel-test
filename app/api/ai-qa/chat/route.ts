12ada04d9e3564556b5ac7735b0341ad91b4ccac
glt <584946709@qq.com>
2026-01-31 20:20:06 +0800

    add AI question

--- app/api/ai-qa/chat/route.ts
+++ app/api/ai-qa/chat/route.ts
@@ -37,9 +37,10 @@ export async function POST(req: NextRequest) {
         let model;
         if (process.env.GOOGLE_API_KEY) {
              model = new ChatGoogleGenerativeAI({
-                model: "gemini-pro",
+                model: "gemini-1.5-flash-latest",
                 apiKey: process.env.GOOGLE_API_KEY,
                 maxOutputTokens: 2048,
+                apiVersion: "v1beta",
             });
         } else {
              model = new ChatOpenAI({
